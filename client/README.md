9bb55c0c5d861895bd1d581732a9a040a476c018
Fitzroy Woodruff <fitzroywoodruff@gmail.com>
2022-12-31 03:11:32 -0500

    Initial setup and commit. Rough project outline

---
+++ client/README.md
@@ -0,0 +1,10 @@
+# React MySQL Full stack blog
+
+## This project uses/will use the following Stack:
+
+- React
+- MySQL
+- Node
+- Express
+- Tailwindcss
+- Google Fonts
---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="App">
+			<h1 className=" font-semibold text-3xl font-Oswald">Home</h1>
+		</div>
+	);
+}
+
+export default App;
